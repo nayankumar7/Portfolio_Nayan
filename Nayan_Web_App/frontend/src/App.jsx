a1d8cdc1daf08e459d2743b37f6d1d00d2c71f7a
nayankumar7 <nkvmp19@gmail.com>
2025-12-21 15:07:07 +0530

    add react router dom

--- Nayan_Web_App/frontend/src/App.jsx
+++ Nayan_Web_App/frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
-
 import './App.css'
+import Home from './Componants/HomePage/Home'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
     </>
   )
 }
